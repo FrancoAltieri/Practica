13460571d2096af82a807dbe6b594f3c2c8d8dfa
FrancoAltieri <94620664+FrancoAltieri@users.noreply.github.com>
2022-06-20 23:02:37 -0300

    Update Correccion-1-facui.c

--- Ivan/Correcciones Facundo/Correccion-1-facui.c
+++ Ivan/Correcciones Facundo/Correccion-1-facui.c
@@ -1,4 +1,13 @@
-Actividad 1
+//Actividad 1
+//Determinar el tipo de dato del resultado de las siguientes expresiones:
+//1. (x>y)
+//2. 1.0 + sin(x)
+//3. (x=1)
+//4. (x>=2)&&(z ==3)
+//5. (x==1)
+//6. (10 - 3% 8 + 6 / 4)
+//7. ( (1 + 3% 2 + 6 * 4 )+2)
+//8. (x=34) + 1
             a. bool
             b. double 
             c. int
@@ -9,7 +18,8 @@ Actividad 1
             h. int
            
            
-Actividad 2
+//Actividad 2
+//Ingresar un valor entero desde el teclado y mostrar por pantalla el valor ingresado.
             
             #include <stdio.h>
             #include <stdlib.h>
@@ -24,7 +34,12 @@ Actividad 2
             }
 
 
-Acrividad 3 y 4
+//Acrividad 3 y 4
+//Desarrollar un algoritmo que permita leer dos numeros enteros por teclado y emitir los resultados de las siguientes
+//operaciones:
+//1. La suma de ambos.
+//2. La resta del primero menos el segundo.
+//3. La multiplicacion de ambos.
 
             #include <stdio.h>
             #include <stdlib.h>
